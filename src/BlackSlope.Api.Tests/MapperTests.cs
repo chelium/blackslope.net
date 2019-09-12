1225dd50d032c50bed94114bfbe506f41639d665
Chris He <chris.he@slalom.com>
2019-09-12 10:27:29 -0500

    Cleanup; Add mapper test; Add package info

---
+++ src/BlackSlope.Api.Tests/MapperTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace BlackSlope.Api.Tests
+{
+    public class MapperTests
+    {
+        private readonly IServiceCollection _services;
+
+        public MapperTests()
+        {
+            _services = new ServiceCollection();
+            _services.AddAutoMapper();
+        }
+
+        [Fact]
+        public void Mapper_Successful()
+        {
+            var serviceProvider = _services.BuildServiceProvider();
+            var mapper = (IMapper)serviceProvider.GetService(typeof(IMapper));
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
--- src/BlackSlope.Api/Operations/Movies/MapperProfiles/CreateMovieRequestProfile.cs
+++ src/BlackSlope.Api/Operations/Movies/MapperProfiles/CreateMovieRequestProfile.cs
@@ -9,7 +9,7 @@ namespace BlackSlope.Api.Operations.Movies.MapperProfiles
         public CreateMovieRequestProfile()
         {
             CreateMap<CreateMovieViewModel, MovieDomainModel>()
-                .ForMember(x => x.Id, c => c.Ignore())
+                .ForMember(x => x.Id, opt => opt.Ignore())
                 .ReverseMap();
         }
     }
